91c58d4309aed4f6f74ca1fd661083e6fa61b354
Archit Singh <qarsingh@amazon.com>
2024-01-22 20:12:25 +0530

    Added implementation of overloading + operator with vector as non-member function

--- The_Cpp_Programming_Language/3_Abstraction_Mechanisms/copy_constructor.cpp
+++ The_Cpp_Programming_Language/3_Abstraction_Mechanisms/copy_constructor.cpp
@@ -24,12 +24,34 @@ public:
   //[]operator overloading
   double &operator[](int i) { return elem[i]; }
 
+  //+ operator overloading
   int size() { return sz; }
 
 private:
   int sz;
   double *elem;
 };
+
+// NOTE : We can't declare this function inside the Vector class
+// Reason : We'll get the below error on compilation :
+// copy_constructor.cpp:27:10: error: overloaded 'operator+' must be a unary or
+// binary operator (has 3 parameters)
+//  Vector operator+(Vector &a, Vector &b) {
+// Member functions by default have an implicit parameter this. Now + is a
+// binary operator. But arguments passed to this function are 3 : this, a and b
+// to solve this problem, we'll have to define operator+ outside the Vector
+// class i.e. making this a non-member function
+// now why can't an operator expect 3 parameters?
+// there is only one such operator ? but it's not possible to overload ?
+// parameter in c++ by definition
+Vector operator+(Vector &a, Vector &b) {
+  if (a.size() != b.size())
+    throw out_of_range("Array sizes are not equal!!");
+  Vector res(a.size());
+  for (int i = 0; i < a.size(); i++)
+    res[i] = a[i] + b[i];
+  return res;
+}
 
 // NOTE : A copy constructor and a copy assignment for a class X are typically
 // declared to take an argument
@@ -53,7 +75,8 @@ Vector &Vector::operator=(const Vector &a) {
   elem = p;
   sz = a.sz;
   return *this;
-  //   The name this is predefined in a member function and points to the
+  //   The name this is predefined in a member function and points to the object
+  //   for which the member
   // function is called.
 }
 //
@@ -61,6 +84,7 @@ int main() {
 
   Vector v1{1, 2, 3};
   Vector v2 = v1;
+  Vector v3 = v2 + v1;
 
   cout << "Printing the elments of vector v1\n";
   for (int i = 0; i < v1.size(); i++)
@@ -71,6 +95,11 @@ int main() {
   for (int i = 0; i < v2.size(); i++)
     cout << "v2 : Element at index " << i << " is : " << v2[i]
          << ". Memory Address : " << &v2[i] << "\n";
+
+  cout << "Printing the elments of vector v3\n";
+  for (int i = 0; i < v3.size(); i++)
+    cout << "v3 : Element at index " << i << " is : " << v3[i]
+         << ". Memory Address : " << &v3[i] << "\n";
 
   cout << "Hello World!!";
   return 0;
