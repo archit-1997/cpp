b371d5ff974305f49dd48ce425bcd64e27007662
Archit <architsingh456@gmail.com>
2024-03-01 14:50:46 +0530

    Added code for move semantics

--- CppCon/move_semantics/andreas_fertig/overloads.cpp
+++ CppCon/move_semantics/andreas_fertig/overloads.cpp
@@ -6,6 +6,11 @@ void fun(const std::vector<int> &byConstRef) {
   std::cout << "This is &byConstRef\n";
 }
 
+//&& triggers move semantics
+void fun(std::vector<int> &&byMoveRef) {
+  std::cout << "This is &&moveRef\n";
+}
+
 int main() {
 
   std::vector<int> a{1, 2, 3};
@@ -14,6 +19,17 @@ int main() {
   fun(a);         // pass an lvalue
   fun(b);         // pass a const lvalue
   fun({7, 8, 9}); // pass a temporary object
+
+  // passing an lvalue by static cast : Triggeting an rvalue overload
+  fun(static_cast<std::vector<int> &&>(a));
+
+  // NOTE : static_cast<std::vector<int> && has type-casted a normal vector
+  // so that the memory allocated can be captured by triggering move semantics.
+  // Now, because writing such a big word is so tedious, there's a small
+  // abbreviation for it : std::move()
+
+  // this is also move semantics
+  fun(std::move(a));
 
   return 0;
 }
